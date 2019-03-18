5f1c3e1c204dfdc1f9716feda6716fbbb4ded0cb
Farzad <farzad@4thirteen.com.my>
2019-03-18 17:01:26 +0800

    Refinde footer style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,15 @@
 
-<footer class="p-3 d-flex row align-items-center">
-    <div class="copyright col-12 col-md-6 mt-4 text-left">
-        Copyright {{ date('Y') }} -
-        A <a target="_blank" href="https://www.code413.io" class="dash-underline">
-            Code413</a> Product
-    </div>
-    
-    <div class="ml-auto col-12 col-md-6 mt-4 text-left text-md-right">
-        <a href="/terms">Terms</a>
-    </div>
+<footer class="p-2 p-sm-3 flex-column flex-grow align-items-center justify-content-center">
+        <div class="row">
+            <div class="copyright col-12 col-md-6 text-left">
+                Copyright {{ date('Y') }} -
+                A <a target="_blank" href="https://www.code413.io" class="dash-underline">
+                    Code413</a> Product
+            </div>
+
+            <div class="ml-auto col-12 col-md-6 text-left text-md-right mt-2 mt-md-0">
+                <a href="/terms">Terms and Conditions</a>
+            </div>
+        </div>
+
 </footer>
